7ceb7db04bca80a6d0dc0934875c8b5e50694d6f
Allen <arusman1987@gmail.com>
2023-05-14 11:43:35 +0800

    sa wakas gumana na ang main carousel

--- hotel/src/components/Card.jsx
+++ hotel/src/components/Card.jsx
@@ -13,8 +13,8 @@ export default function Card({ imageUrl, title, description }) {
   );
 }
 
-Card.defaultProps = {
-  imageUrl: "https://cdn.discordapp.com/attachments/1084710350806585405/1106465630519054366/Soggywaffles_Rose_of_blackpink_in_a_Bboy_stance_background_fore_86f3e494-dbe5-4d16-8c02-9a5081699e37.png",
-  title: "PropTypes.string.isRequired",
-  description: "PropTypes.string.isRequired",
-};
+// Card.defaultProps = {
+//   imageUrl: "https://cdn.discordapp.com/attachments/1084710350806585405/1106465630519054366/Soggywaffles_Rose_of_blackpink_in_a_Bboy_stance_background_fore_86f3e494-dbe5-4d16-8c02-9a5081699e37.png",
+//   title: "PropTypes.string.isRequired",
+//   description: "PropTypes.string.isRequired",
+// };
--- hotel/src/components/MainCar.jsx
+++ hotel/src/components/MainCar.jsx
@@ -1,56 +1,83 @@
 import React, { useState } from "react";
 import Card from "./Card";
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 export default function MainCar(props) {
   const [currentIndex, setCurrentIndex] = useState(0);
-  
 
-  const handlePrevious = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? 4 : prevIndex - 1));
-  };
+                  // event handlers
+                  // ? ternary operators shorthand of if/else
 
   const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 4 ? 0 : prevIndex + 1));
+    setCurrentIndex(currentIndex === 0 ? props.cards.length - 1 : currentIndex -1);
   };
 
-  const images = [props.img1, props.img2, props.img3, props.img4, props.img5];
+  const handlePrevious = () => {
+    setCurrentIndex(currentIndex === props.cards.length - 1 ? 0 : currentIndex + 1);
+  };
 
   return (
-    <div className="card-carousel">
-      <div className="card-group d-flex flex-row overflow-auto">
-        <button className="carousel-button prev-button" onClick={handlePrevious}>
-          <FaChevronLeft />
-        </button>
-        <div className="card-carousel-container">
-          <div
-            className="card-group d-flex flex-row"
-            style={{ transform: `translateX(-${currentIndex * 20}%)` }}
-          >
-            {images.map((imageUrl, index) => (
-              <Card
-                key={index}
-                imageUrl={imageUrl}
-                title={`Card ${index + 1}`}
-                description={`This is a description for card ${index + 1}.`}
-              />
-            ))}
-          </div>
-        </div>
-        <button className="carousel-button next-button" onClick={handleNext}>
-          <FaChevronRight />
-        </button>
+    <div className="mama">
+        <div className="card5">
+        <Card
+          imageUrl={props.cards[(currentIndex + 1) % props.cards.length].imageUrl}
+          title={props.cards[(currentIndex + 1) % props.cards.length].title}
+          description={props.cards[(currentIndex + 1) % props.cards.length].description}
+        />
       </div>
+      <div className="card1">
+        <Card
+          imageUrl={props.cards[(currentIndex + props.cards.length - 1) % props.cards.length].imageUrl}
+          title={props.cards[(currentIndex + props.cards.length - 1) % props.cards.length].title}
+          description={props.cards[(currentIndex + props.cards.length - 1) % props.cards.length].description}
+        />
+      </div>
+      <div className="card2">
+        <Card
+          imageUrl={props.cards[currentIndex].imageUrl}
+          title={props.cards[currentIndex].title}
+          description={props.cards[currentIndex].description}
+        />
+      </div>
+      <div className="card3">
+        <Card
+          imageUrl={props.cards[(currentIndex + 1) % props.cards.length].imageUrl}
+          title={props.cards[(currentIndex + 1) % props.cards.length].title}
+          description={props.cards[(currentIndex + 1) % props.cards.length].description}
+        />
+      </div>
+      <div className="card4">
+        <Card
+          imageUrl={props.cards[(currentIndex + 1) % props.cards.length].imageUrl}
+          title={props.cards[(currentIndex + 1) % props.cards.length].title}
+          description={props.cards[(currentIndex + 1) % props.cards.length].description}
+        />
+      </div>
+      <button className="carousel-button prev-button" onClick={handlePrevious}>
+        Prev
+      </button>
+      <button className="carousel-button next-button" onClick={handleNext}>
+        Next
+      </button>
     </div>
   );
 }
 
-
 MainCar.defaultProps = {
-  img1: "https://cdn.discordapp.com/attachments/1084710350806585405/1106465630519054366/Soggywaffles_Rose_of_blackpink_in_a_Bboy_stance_background_fore_86f3e494-dbe5-4d16-8c02-9a5081699e37.png",
-  img2: "https://cdn.discordapp.com/attachments/1084710350806585405/1106465630519054366/Soggywaffles_Rose_of_blackpink_in_a_Bboy_stance_background_fore_86f3e494-dbe5-4d16-8c02-9a5081699e37.png",
-  img3: "https://cdn.discordapp.com/attachments/1084710350806585405/1106465630519054366/Soggywaffles_Rose_of_blackpink_in_a_Bboy_stance_background_fore_86f3e494-dbe5-4d16-8c02-9a5081699e37.png",
-  img4: "https://cdn.discordapp.com/attachments/1084710350806585405/1106465630519054366/Soggywaffles_Rose_of_blackpink_in_a_Bboy_stance_background_fore_86f3e494-dbe5-4d16-8c02-9a5081699e37.png",
-  img5: "https://cdn.discordapp.com/attachments/1084710350806585405/1106465630519054366/Soggywaffles_Rose_of_blackpink_in_a_Bboy_stance_background_fore_86f3e494-dbe5-4d16-8c02-9a5081699e37.png",
+  cards: [
+    {
+      imageUrl: "https://cdn.discordapp.com/attachments/1084710350806585405/1103306736506511390/Soggywaffles_Lisa_of_blackpink_in_a_Bboy_stance_background_fore_31917df4-a88f-488c-b6ab-fbc8ab70af76.png",
+      title: "Card 1",
+      description: "Description for Card 1",
+    },
+    {
+      imageUrl: "https://cdn.discordapp.com/attachments/1084710350806585405/1106465580116099082/Soggywaffles_Rose_of_blackpink_in_a_Bboy_stance_background_fore_77c18c0b-8d7e-4899-a833-23e443e2fb96.png",
+      title: "Card 2",
+      description: "Description for Card 2",
+    },
+    {
+      imageUrl: "https://cdn.discordapp.com/attachments/1084710350806585405/1103304949405864036/Soggywaffles_jenny_of_blackpink_all_in_a_Bboy_stance_background_dc42dda6-f395-4be2-b0df-ab52fa85e7dc.png",
+      title: "Card 3",
+      description: "Description for Card 3",
+    },
+  ],
 };
-
